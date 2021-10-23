41ad73642b6786f18cc60186053aa314eaff8dc8
a18188916 <kucherenko.a.vl@sberbank.ru>
2021-10-23 22:47:09 +0300

    merge with front

--- ml/classifier.py
+++ ml/classifier.py
@@ -13,12 +13,11 @@ photoPath = sys.argv[3]
 resultFolderName = 'ml/results/search-' + searchId + '/'
 
 if not os.path.exists(resultFolderName):
-    print("test: " + resultFolderName + "\n")
     os.mkdir(resultFolderName)
 
 resultFileName =  resultFolderName + "pic-" + photoId + '.json'
 
-jsonResult = """ {"filePaths": [ "file-storage/OUd_98776281_gettyimages-521697453.jpg", "file-storage/QmQ_98776281_gettyimages-521697453.jpg"]} """
+jsonResult = """ {"filePaths": [ "file-storage/OUd_98776281_gettyimages-521697453.jpg"]} """
 
 with open(resultFileName, 'w+') as f:
     sys.stdout = f
